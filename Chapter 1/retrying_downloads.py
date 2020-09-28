4c1a34bc6c066abb43c14040cadece1a5f24b660
Antonio Zea Jr <spark343@pop-os.localdomain>
2020-09-27 22:58:57 -0400

    Changed the website to be checked.

--- Chapter 1/retrying_downloads.py
+++ Chapter 1/retrying_downloads.py
@@ -10,13 +10,15 @@ def download(url, num_retries =2):
         print("Download error:", e.reason)
         html = None
         if num_retries > 0:
+            # Currently, Forbidden error code is raised probably due to lack of
+            # user agent is not set
             if hasattr(e, "code") and 500 <= e.code < 600:
                 return download(url, num_retries - 1)
     return html
 
 
 def main():
-    download("http://example.webscraping.com")
+    download("http://httpstat.us/500")
 
 
 if __name__ == "__main__":
