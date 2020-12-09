257459589a60361369a57e022707afe3a12e0825
Antonio Zea Jr <halosprtn117@gmail.com>
2020-12-09 02:43:48 -0500

    Final script

--- Personal/twic/twic.py
+++ Personal/twic/twic.py
@@ -11,16 +11,7 @@ webpage_response = requests.get(twic_site, headers = headers)
 soup = BeautifulSoup(webpage_response.content, "html.parser")
 href_tags = soup.find_all("a", href = True)
 
-#for tag in href_tags:
-#    if "PGN" in tag:
-#        print(tag) 
-#print(zip_tags)
-
 zip_tags = [tag for tag in href_tags if "PGN" in tag]
-
-#for html in zip_tags:
-    #re.findall('"([^"]*)"', html)
-#    print(type(html))
 
 htmls = [re.findall('"([^"]*)"', str(html))[0] for html in zip_tags]
 from urllib.request import urlopen
@@ -31,9 +22,3 @@ for url in htmls:
     with urlopen(url) as zipresp:
         with ZipFile(BytesIO(zipresp.read())) as zfile:
             zfile.extractall()
-
-    #r = requests.get(url)
-    #z = zipfile.ZipFile(io.BytesIO(r.content))
-    #z.extractall()
-    #filename = url.split("/")[-1:][0]
-    #open(filename,"wb").write(r.content)
